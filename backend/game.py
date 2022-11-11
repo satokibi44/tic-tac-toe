eb7e138a479e1688072df0329e41493c6ceabf6c
satokibi44 <nanasesaitoh@gmail.com>
2022-11-11 16:50:40 +0900

    　

--- backend/game.py
+++
@@ -1,9 +0,0 @@
-from player import CornerPlayer, GreedyPlayer, EdgePlayer, MiniMaxPlayer, Corner1_Edge2_Ungreedy3
-
-Reversi = {
-    # add player
-    'GREEDY': GreedyPlayer(),
-    'Corner': CornerPlayer(),
-    'MINMAX': MiniMaxPlayer(depth=3),
-    'Edge': EdgePlayer(),
-    'Chimera': Corner1_Edge2_Ungreedy3(), }
--- terraform/ecr.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_ecr_repository" "tic_tac_toe" {
-  name = "tic_tac_toe"
-}
